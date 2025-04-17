96d3c6f2c46cecc5bf0d32879d69687cf1769dfa
sachin634 <78214246+sachin634@users.noreply.github.com>
2025-04-17 22:39:08 +0530

    Update Account.java

--- src/main/java/com/bankmgmt/app/entity/Account.java
+++ src/main/java/com/bankmgmt/app/entity/Account.java
@@ -7,6 +7,9 @@ public class Account {
     private Double balance;
     private String email;
 
+	public Account(){
+		
+	}
     // Constructors, getters, and setters
 
     public Account(Integer id, String accountHolderName, String accountType, Double balance, String email) {
@@ -16,6 +19,46 @@ public class Account {
         this.balance = balance;
         this.email = email;
     }
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+
+	public void setAccountHolderName(String accountHolderName) {
+		this.accountHolderName = accountHolderName;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
     // TODO: Add getters and setters
 
